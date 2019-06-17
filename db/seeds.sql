7a557e7b3f80acdb7444aa042069c8694f57bcb1
Tyler <williamstyler707@gmail.com>
2019-06-17 13:56:59 -0400

    Repo Set Up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Donut Burger', 0);
